dcf765ade1e8e0cdef496e40e5e9f3f04b451a7d
qoqomi <energyy044@gmail.com>
2026-03-17 00:02:24 +0900

    다리는 지나는 트럭(Ing)

---
+++ StackQueue/seungyeon/다리를 지나는 트럭.py
@@ -0,0 +1,21 @@
+def solution(bridge_length, weight, truck_weights):
+    time = 0
+    bridge = [0] * bridge_length
+
+    current_weight = 0
+
+    while len(truck_weights) > 0:
+        time += 1
+
+        current_weight -= bridge.pop(0)
+
+        if current_weight + truck_weights[0] <= weight:
+            truck = truck_weights.pop(0)
+            bridge.append(truck)
+            current_weight += truck
+        else:
+            bridge.append(0)
+
+    time += bridge_length
+
+    return time
